2acb14908f5ee80abb7ae736233dea008bceb496
Terri Hall <webdev.terrihall@gmail.com>
2018-01-31 19:25:50 -0700

    Add files via upload

--- Thall-Scriptingl.py
+++ Thall-Scriptingl.py
@@ -10,28 +10,30 @@
 # Tested OS:       This code was written and tested to work with Windows 10.
  
 
-import shutil, os, sys, time
+import shutil, os, time
 
-def main():
-    src = 'C:\Users\webde\Desktop\FolderA'
-    dest = 'C:\Users\webde\Desktop\FolderB'
-    lastFileUpdate = os.path.getmtime(sourceFile)
-
+def checkFileMove(src, dest):
+    currentTime = time.time()
+    
 listFile = os.listdir(src)
 for files in listFile:
+    
     if files.endswith(".txt"):
-        sourceFile = os.path.join(src + files)  #This checks for mod time
-    lastFileUpdate = os.path.getmtime(sourceFile)
-    print (lastFileUpdate)
-
-currentTime = time.time()
-modTime = currentTime - lastFileUpdate  #time difference between current time and mod time
-
-if modTime < 86400:
-    shutil.move(sourceFile, dest)
-    print (sourceFile + 'Was moved to FolderB')
-else:
-    print ("No files were updated today")
+        sourceFile = os.path.join(src + files)
+        lastFileUpdate = os.path.getmtime(sourceFile)
+        print (lastFileUpdate)
+        modTime = currentTime - lastFileUpdate
+        if modTime < 86400:
+            shutil.move(sourceFile, dest)
+            print (sourceFile + 'Was moved to FolderB')
+        else:
+            print ("No files were updated today")
+    
+def main():
+    src = 'C:\Users\webde\Desktop\FolderA\\'
+    dest = 'C:\Users\webde\Desktop\FolderB\\'
+    checkFileMove(src, dest)
+    
                 
 if __name__=='__main__':
     main()
